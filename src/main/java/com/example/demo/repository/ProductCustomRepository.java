3f42d4878569d01b8ab7262e52d8038504fcb8a0
Song-EunJu <jd06280@naver.com>
2022-01-23 15:57:43 +0900

    [chore] productService 주석 처리

--- src/main/java/com/example/demo/repository/ProductCustomRepository.java
+++ src/main/java/com/example/demo/repository/ProductCustomRepository.java
@@ -1,9 +1,9 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.Product;
-
-import java.util.List;
-
-public interface ProductCustomRepository {
-    List<Product> findAllInnerJoinWithProductImage();
-}
+//package com.example.demo.repository;
+//
+//import com.example.demo.domain.Product;
+//
+//import java.util.List;
+//
+//public interface ProductCustomRepository {
+//    List<Product> findAllInnerJoinWithProductImage();
+//}
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -6,11 +6,13 @@ import com.example.demo.domain.User;
 import com.example.demo.repository.ProductImageRepository;
 import com.example.demo.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class ProductService {
     private ProductRepository productRepository;
     private ProductImageRepository productImageRepository;
